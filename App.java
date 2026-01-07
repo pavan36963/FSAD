8dec1f6e0a6398190852d4094addc04eddbc9b8c
pavan36963 <pavanteja36963@gmail.com>
2026-01-07 09:22:38 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,23 +1,35 @@
-package com.klu.main;
-
+package com.klu;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
-import com.klu.model.Product;
-import com.klu.util.HibernateUtil;
-
 public class App {
     public static void main(String[] args) {
-
+        // CREATE
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = session.beginTransaction();
-
-        Product p = new Product("Laptop", 55000);
-        session.save(p);
-
+        Student s1 = new Student(101, "Ram", "CSE");
+        session.save(s1);
         tx.commit();
         session.close();
-
-        System.out.println("Product inserted successfully");
-    }
-}
+        System.out.println("Student Inserted");
+        
+        // READ
+        session = HibernateUtil.getSessionFactory().openSession();
+        Student s = session.get(Student.class, 101);
+        System.out.println(s.getName() + " " + s.getDept());
+        session.close();
+        // UPDATE
+        session = HibernateUtil.getSessionFactory().openSession();
+        tx = session.beginTransaction();
+        s.setDept("AI & DS");
+        session.update(s);
+        tx.commit();
+        session.close();
+        System.out.println("Student Updated");
+        // DELETE
+        session = HibernateUtil.getSessionFactory().openSession();
+        tx = session.beginTransaction();
+        session.delete(s);
+        tx.commit();
+        session.close();
+        System.out.println("Student Deleted");
+    }}
--- HibernateUtil (1).java
+++ HibernateUtil (1).java
@@ -3,9 +3,13 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static SessionFactory factory;
-
     static {
-        factory = new Configuration().configure().buildSessionFactory();
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+            System.out.println("Hibernate SessionFactory Created");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public static SessionFactory getSessionFactory() {
         return factory;
